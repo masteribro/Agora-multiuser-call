2f1d9b1d7c44459b6ccd05206b872e2a193e90f2
Masteribrahim1 <mohammed.ibrahim@dreamlabs.com.ng>
2024-11-15 06:54:35 +0100

    first commit

---
+++ lib/constant/constant.dart
@@ -0,0 +1,6 @@
+// Fill in the app ID obtained from the Agora console
+const appId = "dabbbc48734a416f912b98f7132a7fc6";
+// Fill in the temporary token generated from Agora Console
+const token = "007eJxTYDD5vrkuWUU9dLFAJcv8/niJRxv+3zD34b3x7U7yus1L+c0UGFISk5KSkk0szI1NEk0MzdIsDY2SLC3SzA2NjRLN05LN5pw0S28IZGR4LqvCwAiFID4LQ0lqcQkDAwBEPB9P";
+// Fill in the channel name you used to generate the token
+const channel = "test";
---
+++ lib/home_page.dart
@@ -0,0 +1,172 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'constant/constant.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  RtcEngine? _engine;
+  bool localUserJoined = false;
+  List<int> users = []; // List to store remote user IDs
+
+  @override
+  void initState() {
+    super.initState();
+    initAgora();
+  }
+
+  @override
+  void dispose() {
+    _dispose();
+    super.dispose();
+  }
+
+  Future<void> initAgora() async {
+    await [Permission.microphone, Permission.camera].request();
+
+    _engine = createAgoraRtcEngine();
+    await _engine?.initialize(RtcEngineContext(appId: appId));
+
+    await _engine?.enableVideo();
+    await _engine?.startPreview();
+
+    _engine?.registerEventHandler(
+      RtcEngineEventHandler(
+        onJoinChannelSuccess: (connection, elapsed) {
+          setState(() {
+            localUserJoined = true;
+          });
+        },
+        onUserJoined: (connection, remoteUid, elapsed) {
+          setState(() {
+            users.add(remoteUid);
+          });
+        },
+        onUserOffline: (connection, remoteUid, reason) {
+          setState(() {
+            users.remove(remoteUid);
+          });
+        },
+      ),
+    );
+
+    await _engine?.joinChannel(
+      token: token,
+      channelId: channel,
+      uid: 0,
+      options: const ChannelMediaOptions(
+        autoSubscribeVideo: true,
+        autoSubscribeAudio: true,
+        publishCameraTrack: true,
+        publishMicrophoneTrack: true,
+        clientRoleType: ClientRoleType.clientRoleBroadcaster,
+      ),
+    );
+  }
+
+  Future<void> _dispose() async {
+    await _engine?.leaveChannel();
+    await _engine?.release();
+  }
+
+  // Build video layout based on the number of users
+  Widget _renderVideoLayout() {
+    if (_engine == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    List<Widget> videoViews = [];
+
+    // Add the local video view
+    videoViews.add(
+      AgoraVideoView(
+        controller: VideoViewController(
+          rtcEngine: _engine!,
+          canvas: const VideoCanvas(uid: 0),
+        ),
+      ),
+    );
+
+    // Add remote user video views
+    for (var uid in users) {
+      videoViews.add(
+        AgoraVideoView(
+          controller: VideoViewController.remote(
+            rtcEngine: _engine!,
+            canvas: VideoCanvas(uid: uid),
+            connection: RtcConnection(channelId: channel),
+          ),
+        ),
+      );
+    }
+
+    // Display full screen for single user
+    if (videoViews.length == 1) {
+      return videoViews[0];
+    }
+
+    // For two users, split screen vertically
+    if (videoViews.length == 2) {
+      return Column(
+        children: [
+          Expanded(child: videoViews[0]),
+          Expanded(child: videoViews[1]),
+        ],
+      );
+    }
+
+    // For 3 or 4 users, use a 2x2 grid
+    if (videoViews.length <= 4) {
+      return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.0,
+        ),
+        itemCount: videoViews.length,
+        itemBuilder: (context, index) {
+          return videoViews[index];
+        },
+      );
+    }
+
+    // For more than 4 users, use a grid layout with increasing columns
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: (videoViews.length / 2).ceil(),
+        childAspectRatio: 1.0,
+      ),
+      itemCount: videoViews.length,
+      itemBuilder: (context, index) {
+        return videoViews[index];
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: _renderVideoLayout(),
+          ),
+          if (!localUserJoined)
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
+      ),
+    );
+  }
+}
